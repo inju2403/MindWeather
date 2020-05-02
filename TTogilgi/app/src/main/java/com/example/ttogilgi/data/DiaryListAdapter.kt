9df950d0f77537b88e977cdc1f0dc159da92d6a3
inju2403 <inju2403@gmail.com>
2020-05-02 12:01:24 +0900

    요일 추가

--- TTogilgi/app/src/main/java/com/example/ttogilgi/data/DiaryListAdapter.kt
+++ TTogilgi/app/src/main/java/com/example/ttogilgi/data/DiaryListAdapter.kt
@@ -6,10 +6,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.ttogilgi.R
 import kotlinx.android.synthetic.main.item_diary.view.*
 import java.text.SimpleDateFormat
+import java.util.*
 
 class DiaryListAdapter (private val list : MutableList<DiaryData>) : RecyclerView.Adapter<ItemViewHolder> () {
 
     private val dateFormat = SimpleDateFormat("MMdd")
+    private val weekdayFormat = SimpleDateFormat("EEE", Locale.ENGLISH)
 
     lateinit var itemClickListener : (itemId : String) -> Unit
 
@@ -31,7 +33,7 @@ class DiaryListAdapter (private val list : MutableList<DiaryData>) : RecyclerVie
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         holder.containerView.summaryView.text = list[position].summary
         holder.containerView.dateView.text = dateFormat.format(list[position].createdAt)
-    //    holder.containerView.dayOfTheWeekView.text =
+        holder.containerView.dayOfTheWeekView.text = weekdayFormat.format(list[position].createdAt)
         holder.containerView.tag = list[position].id
     }
 
