10b8555719549c4c90d5624d6a95ffd2dcb7dab0
inju2403 <inju2403@gmail.com>
2020-05-06 23:21:42 +0900

    DiaryData 속성 변경

--- TTogilgi/app/src/main/java/com/example/ttogilgi/data/DiaryData.kt
+++ TTogilgi/app/src/main/java/com/example/ttogilgi/data/DiaryData.kt
@@ -7,11 +7,15 @@ import java.util.*
 open class DiaryData (
     @PrimaryKey
     var id: String = UUID.randomUUID().toString(),
+    var userName: String = "", //닉네임
     var createdAt: Date = Date(),
     var updatedAt: Date = Date(),
     var content: String = "",
     var summary: String = "",
-    var userId: String = "",
-    var feeling: Int = 0,
+    var happiness: Int = 0,
+    var neutrality: Int = 0,
+    var sadness: Int = 0,
+    var worry: Int = 0,
+    var anger: Int = 0,
     var qna: String = ""
 ) : RealmObject()
